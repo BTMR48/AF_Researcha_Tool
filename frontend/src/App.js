fe46167d0ef25946b9dba046188c7daefec9f2fc
Tharindu Dasun <tharindudasun0@gmail.com>
2022-05-13 14:42:43 +0530

    push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,26 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
 import Header from './components/Header/Header'
+// import PrivateRoute from './Routes/PrivateRoute';
+// import StudentPrivateRoute from './Routes/StudentPrivateRoute';
+
 
 import AdminSignIn from './components/AdminManagement/AdminLogin';
+import AddProgress from './components/ProgressManagement/AddProgress/AddProgress';
+import Levels from './components/ProgressManagement/Levels/Levels';
+import SingleLevel from './components/ProgressManagement/SingleLevel/SingleLevel';
+import UpdateProgress from './components/ProgressManagement/UpdateProgress/UpdateProgress';
+import AddSubmission from './components/SubmissionManagement/AddSubmission/AddSubmission';
+
+import SupervisorSignIn from './components/SupervisorManagement/SupervisorSignIn/SupervisorSignIn'; 
+import SupervisorSignUp from './components/SupervisorManagement/SupervisorSignUp/SupervisorSignUp'; 
+import CosupervisorSignIn from './components/CosupervisorManagement/CosupervisorSignIn/CosupervisorSignIn'; 
+import CosupervisorSignUp from './components/CosupervisorManagement/CosupervisorSignUp/CosupervisorSignUp'; 
+import PanelmemberSignIn from './components/PanelmemberManagement/PanelmemberSignIn/PanelmemberSignIn'; 
+import PanelmemberSignUp from './components/PanelmemberManagement/PanelmemberSignUp/PanelmemberSignUp'; 
+
+import AllSupervisors from './components/UserManagement/SupervisorList/allsupervisors'; 
+
 
 
 function App() {
@@ -11,11 +29,31 @@ function App() {
     <div className="App">
       <Router>
         <div>
+
+            {/* <Header/> */}
+              <Route path="/admin/signin" exact component={AdminSignIn} />
+              <Route  path="/evolution/addProgress"exact component={AddProgress}/>
+              <Route  path="/evolution/levels" exact component={Levels}/>
+              <Route  path="/evolution/level/:id" exact component={SingleLevel}/>
+              <Route  path="/evolution/level/updateProgress/:id" exact component={UpdateProgress}/>
+
+              <Route path="/submission/addSubmission/:id" exact component= {AddSubmission}/>
+
             <Header/>
             
             
             <Route path="/admin/signin" exact component={AdminSignIn} />
-           
+
+
+            <Route path="/supervisor/signin" exact component = {SupervisorSignIn}/>
+            <Route path="/supervisor/signup" exact component = {SupervisorSignUp}/>
+            <Route path="/cosupervisor/signin" exact component = {CosupervisorSignIn}/>
+            <Route path="/cosupervisor/signup" exact component = {CosupervisorSignUp}/>
+            <Route path="/panelmember/signin" exact component = {PanelmemberSignIn}/>
+            <Route path="/panelmember/signup" exact component = {PanelmemberSignUp}/>
+            <Route path="/users/supervisorlist" exact component = {AllSupervisors}/>
+
+       
         </div>
       </Router>
     </div>
